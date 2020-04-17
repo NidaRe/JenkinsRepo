f6c14f61569cb837016191a99ca7e78cccc63900
NidaRe <53389364+NidaRe@users.noreply.github.com>
2020-04-17 15:48:48 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class helloworld {
 
     public static void main(String[] args) {
         System.out.println("This is the first program to be tested by Jenkins Pipeline");
