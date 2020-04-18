90d4c2a823baaa75cf8798eebd0e8a6a5cb2148e
NidaRe <53389364+NidaRe@users.noreply.github.com>
2020-04-18 17:14:57 +0530

    Add files via upload

---
+++ parameterizedDescribeInstance.py
@@ -0,0 +1,14 @@
+import boto3
+import sys
+
+region = sys.argv[1]
+accesskey = sys.argv[2]
+secretkey = sys.argv[3]
+
+client = boto3.client('ec2',region_name=region, aws_access_key_id=accesskey,aws_secret_access_key=secretkey)
+
+ins_data = client.describe_instances()
+for step1 in ins_data['Reservations']:
+	for step2 in step1['Instances']:
+		print(step2['InstanceId'])
+
